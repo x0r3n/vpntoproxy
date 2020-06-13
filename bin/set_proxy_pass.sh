9724c77df8d0f0e63a0e73491f706f7ad1ca761b
x0r3n <adam.jugas@gmail.com>
2020-06-13 09:36:39 +0000

    added check if squidusers file size greater than zero

--- bin/set_proxy_pass.sh
+++ bin/set_proxy_pass.sh
@@ -4,7 +4,10 @@ squid_username="$(echo $SQUID_USER)"
 squid_password="$(echo $SQUID_PASS)"
 
 FILE=/etc/squid/squidusers
-if [ ! -f "$FILE" ]; then
+if [ -s "$FILE" ]; then
+  exit 0;
+else
+
   sleep 1
   htpasswd -c -b /etc/squid/squidusers $squid_username $squid_password
 
